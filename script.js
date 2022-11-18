6c30f6139636752ec1ca7c898a9541bf177188e8
Your Name <you@example.com>
2022-11-18 15:00:48 -0300

    Adicionando tags na tabela

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 //Seleciona os elementos input e button
-var input = document.querySelector('input')
+var input = document.querySelector('.input')
 var button = document.querySelector('.btn')
+
 
 button.onclick = buscarCEP
 
 function buscarCEP(){
+    //capturar conteudo do input
     let cep = input.value
-    input.value=""
-    let tags = ['td','tr','tr','tr','tr','tr','tr','tr']
-    ListaDeTags = []
-    ListaDeTags = CriarTags(tags, ListaDeTags)
 
-    VerificarCEP(cep, ListaDeTags)
-
+    VerificarCEP(cep)
 
 }
+    function VerificarCEP (){
+        let url = `https://viacep.com.br/ws/${cep}/json/`
 
-function VerificarCEP(cep, ListaDeTags){
+    }
+
+function VerificarCEP(cep){
     let url = `https://viacep.com.br/ws/${cep}/json/`
 
     fetch(url)
@@ -25,27 +26,56 @@ function VerificarCEP(cep, ListaDeTags){
         return response.json()
     })
     .then(response =>{
-        ListaDeTags[1].textContent = response.cep
-        ListaDeTags[2].textContent = response.logradouro
-        ListaDeTags[3].textContent = response.complemento
-        ListaDeTags[4].textContent = response.bairro
-        ListaDeTags[5].textContent = response.cidade
-        ListaDeTags[6].textContent = response.uf
-        ListaDeTags[7].textContent = response.ddd
 
-
-        console.log(response)
+        let tags = ['tr','td','td','td','td','td','td','td']
+        let ListaDeTags = []
+        
+        ListaDeTags = CriarTags(tags, ListaDeTags)
+        preencherTds(ListaDeTags, response)
+        
+        inserirNaTela(ListaDeTags)
     })
+
 }
 
 function CriarTags(tags, ListaDeTags){
 
     tags.forEach(textoTag => {
+        //criar tags
         let tag = document.createElement(textoTag)
+        //guarda as tags detro do Lista de tags     
         ListaDeTags.push(tag)
     })
 
     return ListaDeTags
+
+}
+
+function preencherTds(ListaDeTags, response){
+
+        ListaDeTags[1].textContent = response.cep
+        ListaDeTags[2].textContent = response.logradouro
+        ListaDeTags[3].textContent = response.complemento
+        ListaDeTags[4].textContent = response.bairro
+        ListaDeTags[5].textContent = response.localidade
+        ListaDeTags[6].textContent = response.uf
+        ListaDeTags[7].textContent = response.ddd
+
+}
+
+function inserirNaTela(ListaDeTags){
+    let body = document.querySelector('.CorpoTabela')
+
+    body.appendChild(ListaDeTags[0])
+
+    for (let contador = 1; contador < ListaDeTags.length; contador++) {
+
+        ListaDeTags[0].appendChild(ListaDeTags[contador])
+
+    }
+        
+    
+    
 
 }
 
